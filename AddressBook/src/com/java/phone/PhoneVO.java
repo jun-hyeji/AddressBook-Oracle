038739f9389d72317c6dbd1026796ebfb76c3590
hyemyung kim <teajinkim0@naver.com>
2021-03-23 10:31:05 +0900

    PhoneBook

--- AddressBook/src/com/java/phone/PhoneVO.java
+++ AddressBook/src/com/java/phone/PhoneVO.java
@@ -1,38 +1,48 @@
-package com.java.Phone;
+package com.java.phone;
 
-class Phone{
-	private String name;	// 이름
-	private String hp;	// 휴대전화
-	private String tel;		// 집전화
+public class PhoneVO {
 	
-	public Phone(String name, String hp, String tel) {
+	private String name;
+	private String hp;
+	private String tel;
+	
+	public PhoneVO() {
+		super();
+	}
+
+	public PhoneVO(String name, String hp, String tel) {
 		super();
 		this.name = name;
 		this.hp = hp;
 		this.tel = tel;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getHp() {
 		return hp;
 	}
+
 	public void setHp(String hp) {
 		this.hp = hp;
 	}
+
 	public String getTel() {
 		return tel;
 	}
+
 	public void setTel(String tel) {
 		this.tel = tel;
 	}
+
 	@Override
 	public String toString() {
-		return "[이름 : " + name + ", 휴대전화 : " + hp + ", 집전화 : " + tel + "]";
+		return "PhoneInfo [name=" + name + ", hp=" + hp + ", tel=" + tel + "]";
 	}
-	
-	
 }
